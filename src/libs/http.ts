3e108b263fe6c71b79c28364bfddf463ae75035c
原罪 <531362022@qq.com>
2021-11-24 23:27:21 +0800

    use foca-axios

--- src/libs/http.ts
+++ src/libs/http.ts
@@ -1,6 +1,6 @@
 import { message } from 'antd';
 import axios, { AxiosError } from 'axios';
-import { enhanceAxios } from 'foca-axios-enhancer';
+import { enhance } from 'foca-axios';
 
 const instance = axios.create({
   // codesandbox里需要使用https，否则请求出错
@@ -13,7 +13,7 @@ instance.interceptors.response.use(undefined, (err: AxiosError) => {
   return Promise.reject(err);
 });
 
-export const http = enhanceAxios(instance, {
+export const http = enhance(instance, {
   cache: {
     enable: false,
   },
