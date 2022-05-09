39211be1a6cca450e89e467588676b6b41425d6f
geekact <fanwenhua1990@gmail.com>
2022-05-09 12:36:24 +0800

    upgrade to react18

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import './store';
 
 import { FocaProvider } from 'foca';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom/client';
 import App from './App';
 import zhCN from 'antd/es/locale/zh_CN';
 import { ConfigProvider } from 'antd';
 
-ReactDOM.render(
+const container = document.getElementById('root')!;
+const root = ReactDom.createRoot(container);
+
+root.render(
   <ConfigProvider locale={zhCN}>
     <FocaProvider>
       <App />
     </FocaProvider>
   </ConfigProvider>,
-  document.getElementById('root'),
 );
